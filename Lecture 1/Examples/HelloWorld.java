68d74d9854f4cd2c30260f1dc814920968087546
andrzej <andrzejkaluza92@gmail.com>
2025-10-14 14:16:23 +0200

    Poprawki

--- Lecture 1/Examples/HelloWorld.java
+++ Lecture 1/Examples/HelloWorld.java
@@ -1,7 +1,16 @@
-package Examples;
+package examples;
 
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        SecondClass secondClass = new SecondClass();
+        SecondClass.print2();
+        String name2 = secondClass.getName2();
+        System.out.println(name2);
+        secondClass.print();
+        secondClass.getName().toLowerCase();
+        secondClass.setName("Java L1");
+        System.out.println(secondClass.getName());
+
     }
 }
